22364a3fc0ef97f4397c3c7f2d5d562b61e3d3a2
Mogy <norou6@gmail.com>
2024-03-18 23:34:12 +0000

    react state

--- my-first-react-app/src/fruits.js
+++ my-first-react-app/src/fruits.js
@@ -1,9 +1,27 @@
-export default function Fruits({fruits}){
-    const displayFruits = ()=> fruits.map(fruit => <li>{fruit}</li>)
-    return <>
-        <h2>Fruits : </h2>
-        <ul>
-            {displayFruits()}
-        </ul>
-        </>
+// export default function Fruits({fruits}){
+//     const displayFruits = ()=> fruits.map(fruit => <li>{fruit}</li>)
+//     return <>
+//         <h2>Fruits : </h2>
+//         <ul>
+//             {displayFruits()}
+//         </ul>
+//         </>
+// }
+import {Component} from "react";
+
+export default class Fruits extends Component{
+    constructor(fruits) {
+        super(fruits);
+    }
+    displayFruits = () => this.fruits.map(fruit => <li>{fruit}</li>)
+
+    render() {
+        return <>
+                   <h2>Fruits : </h2>
+                    <ul>
+                        {this.displayFruits()}
+                    </ul>
+                    </>
+    }
 }
+
