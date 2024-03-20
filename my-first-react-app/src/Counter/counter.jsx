f3c80e320e3e8262cd04da7be379b011d84a78cb
Mogy <norou6@gmail.com>
2024-03-20 16:33:26 +0000

    events in react with Hooks

--- my-first-react-app/src/Counter/counter.jsx
+++ my-first-react-app/src/Counter/counter.jsx
@@ -1,24 +1,39 @@
-import {Component} from "react";
+import {useState} from "react";
 
-export default class Counter extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {counter: 0,
-        date : undefined,
-        }
-    }
-
-    handelClick = ()=>{
-        this.setState(
-            prevState => {
-
-                return {counter: prevState.counter+1}
-            }
-        )
-    }
-    render() {
-        return <div>
-            <button onClick={this.handelClick}>Click click</button>
-            it passed {this.state.counter} seconds ago</div>
-    }
+export default function Counter(){
+    const [counter, setCount] = useState(0);
+    return  <div>
+        <button onClick={()=>{
+            setCount(prevState => {
+                return prevState + 1
+            })
+        }}>Click click</button>
+        it passed {counter} seconds ago</div>
 }
+
+
+
+// import {Component} from "react";
+//
+// export default class Counter extends Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {counter: 0,
+//         date : undefined,
+//         }
+//     }
+//
+//     handelClick = ()=>{
+//         this.setState(
+//             prevState => {
+//
+//                 return {counter: prevState.counter+1}
+//             }
+//         )
+//     }
+//     render() {
+//         return <div>
+//             <button onClick={this.handelClick}>Click click</button>
+//             it passed {this.state.counter} seconds ago</div>
+//     }
+// }
