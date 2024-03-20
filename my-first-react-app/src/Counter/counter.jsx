71cd53c37f9ae90f8810875a224a70703ea44599
Mogy <norou6@gmail.com>
2024-03-20 23:57:19 +0000

    form  in react with Hooks useState and userEffect

--- my-first-react-app/src/Counter/counter.jsx
+++ my-first-react-app/src/Counter/counter.jsx
@@ -1,46 +1,61 @@
-// import {useState} from "react";
-//
-// export default function Counter(){
-//     const [counter, setCount] = useState(0);
-//     return  <div>
-//         <button onClick={()=>{
-//             setCount(prevState => {
-//                 return prevState + 1
-//             })
-//         }}>Click click</button>
-//         it passed {counter} seconds ago</div>
-// }
+import {useEffect, useState} from "react";
+
+export default function Counter(){
+    const [counter, setCount] = useState(0);
+    const [time, setTime] = useState(new Date());
+    let timer =  null
+    useEffect(() => {
+        timer = setInterval(()=>{
+            setTime(new Date());
+        }, 1000)
+    return () => clearInterval(timer)
+    }, []);
+
+    useEffect(() => {
+        console.log('component did updated !')
+    }, [counter, time]);
 
 
-
-import {Component, useState} from "react";
-
-export default class Counter extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {counter: 0,
-        date : undefined,
-        }
-    }
-
-    componentDidMount() {
-        console.log(this.state)
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log(prevState, this.state);
-    }
-
-    handelClick = ()=>{
-        this.setState(
-            prevState => {
-
-                return {counter: prevState.counter+1}
-            }
-        )
-    }
-    render() {
-        return <div>
-            <button onClick={this.handelClick}>Click click</button>
-            it passed {this.state.counter} seconds ago</div>
-    }
+    return  <div>
+        <button>Click click</button>
+        it passed {time.toTimeString()}seconds ago</div>
 }
+
+
+//
+// import {Component, useState} from "react";
+//
+// export default class Counter extends Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {counter: 0,
+//         date : undefined,
+//         }
+//     }
+//
+//     componentDidMount() {
+//         console.log(this.state)
+//     }
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         console.log(prevState, this.state);
+//     }
+//
+//     shouldComponentUpdate(nextProps, nextState, nextContext) {
+//         return nextState.counter !== this.state.counter;
+//     }
+//
+//     handelClick = ()=>{
+//         console.log(this.state.counter)
+//         this.setState(
+//             prevState => {
+//                 return {counter: prevState.counter+1}
+//             }
+//         )
+//     }
+//     render() {
+//         return <div>
+//             <button onClick={this.handelClick}>Click click</button>
+//             it passed {this.state.counter} seconds ago</div>
+//     }
+// }
+//
--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ToogleName from "./Toogle/toogleName";
+import Counter from "./Counter/counter";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const fruits = ['dela7', 'le3neb', 'teffah', 'lbanan']
 root.render(
   <React.StrictMode>
-      <ToogleName/>
+      <Counter/>
   </React.StrictMode>
 );
 
