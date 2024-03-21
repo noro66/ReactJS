9fe87bf475814c611da9ea1705735e307f587efe
Mogy <norou6@gmail.com>
2024-03-21 22:01:23 +0000

    search in  react

--- my-first-react-app/src/tps/Store/Product.jsx
+++ my-first-react-app/src/tps/Store/Product.jsx
@@ -1,14 +1,12 @@
 export default function Product({product})
 {
-
-    console.log(product)
     return  <tr>
       <td>{product.id}</td>
          <td>{product.title}</td>
          <td>{product.price}</td>
          <td>{product.description}</td>
          <td>{product.category}</td>
-         <td>{product.image}</td>
-        <td>{product.rating.rate}{ ` (${product.rating.count}) rater`}</td>
+         <td><img width={250} height={350} src={product.image} alt="{product.title}"/></td>
+        <td><span className="badge badge-pill bg-secondary-subtle text-black">{product.rating.rate}{`/(${product.rating.count} rater)`}</span></td>
      </tr>
 }
--- my-first-react-app/src/tps/Store/ProductList.jsx
+++ my-first-react-app/src/tps/Store/ProductList.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import Product from "./Product";
 
 export default function ProductList() {
-    const [productList, setProductList] = useState([]);
+    let [productList, setProductList] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
     const getProducts = () => {
         fetch('https://fakestoreapi.com/products')
             .then(response => response.json())
@@ -15,14 +16,35 @@ export default function ProductList() {
     }, []);
 
    const displayProducts = () =>{
+       if (searchInput !== ''){
+           const productListTemp  =  productList.filter(product => {
+               return product.title.startsWith(searchInput)
+           })
+           return productListTemp.map((product)=>{
+               return <Product product={product}/>
+           })
+       }
         return productList.map((product)=>{
           return <Product product={product}/>
         })
     }
-    // displayProducts();
+
+    const handledSearch = (e) => {
+        e.preventDefault();
+        setSearchInput(document.getElementById('searchInput').value);
+    }
+
     return (
         <div className='container mt-3'>
-            <h1>Products list</h1>
+            <h1>Products list : </h1>
+            <h2>Search : </h2>
+            <form onSubmit={handledSearch}>
+            <div className="form-group">
+                <label for="searchInput">Search For Products : </label>
+                <input type="text" name="" id="searchInput" className="form-control w-25 mb-2" placeholder="" aria-describedby="helpId"/>
+                <input type="submit" className="bg-primary" value="Search"/>
+            </div>
+            </form>
             <table className="table">
                 <thead>
                 <tr>
