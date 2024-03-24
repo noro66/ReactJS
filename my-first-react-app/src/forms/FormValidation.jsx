a184c7dc2e00afbc223d5f5de09104bd6ff7b151
Mogy <norou6@gmail.com>
2024-03-24 00:02:11 +0000

    validation in react 03

--- my-first-react-app/src/forms/FormValidation.jsx
+++ my-first-react-app/src/forms/FormValidation.jsx
@@ -1,103 +1,115 @@
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function FormValidation()
-{
+export default function FormValidation() {
     const name = useRef();
     const email = useRef();
     const message = useRef();
     const conditions = useRef();
     const [errors, setErrors] = useState({});
-    const [isFormSubmited, setisFormSubmited] = useState(false);
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
     const checkInput = (input) => {
-        let  isFormValid = true;
-        if (input.current.type === 'checkbox'){
-            if (!input.current.checked) {
-                setErrors(prevState => ({   ...prevState,  [input.current.id]: 'conditions must be checked' }));
-                isFormValid = false;
-            }
-        }else if((input.current.value).trim() === '') {
-            setErrors(prevState => ({...prevState, [input.current.id]: 'field required'}));
-            isFormValid = false;
-        }else if (input.current.id === 'email'){
-            if (!input.current.value.match(/^\S+@\S+\.\S+$/)){
-                setErrors(prevState => ({...prevState,[input.current.id]: 'Invalid Email'}));
-                isFormValid = false;
-            }
-        }
-        return isFormValid;
-    };
+        let validationFailed = false;
 
-    const resetForm = ()=>{
-            name.current.value = ''
-            email.current.value = ''
-            message.current.value = ''
-            conditions.current.checked = false;
+        if (input.current.type === 'checkbox') {
+            if (!input.current.checked) {
+                input.current.style.border = "1px solid red";
+                setErrors(prevState => ({ ...prevState, [input.current.id]: 'conditions must be checked' }));
+                validationFailed = true;
+            } else {
+                input.current.style.border = "1px solid green";
+            }
+        } else if ((input.current.value).trim() === '') {
+            input.current.style.border = "1px solid red";
+            setErrors(prevState => ({ ...prevState, [input.current.id]: 'field required' }));
+            validationFailed = true;
+        } else if (input.current.id === 'email') {
+            if (!input.current.value.match(/^\S+@\S+\.\S+$/)) {
+                input.current.style.border = "1px solid red";
+                setErrors(prevState => ({ ...prevState, [input.current.id]: 'Invalid Email' }));
+                validationFailed = true;
+            } else {
+                input.current.style.border = "1px solid green";
+            }
+        } else {
+            input.current.style.border = "1px solid green";
+        }
+
+        return validationFailed;
     }
+
     function handelSubmit(e) {
         e.preventDefault();
-        console.log(errors);
-            setErrors([]);
-            let  checkValid;
-            const refs =[name, email, message, conditions]
-            for (let i = 0; i < refs.length ;i++){
-               checkValid =  checkInput(refs[i]);
-            }
-            if (checkValid){
-                setisFormSubmited(true)
-                resetForm();
-            }
+        setIsFormSubmitted(false);
+        setErrors({});
+        let formIsValid = true;
 
+        const refs = [name, email, message, conditions];
+        refs.forEach(ref => {
+            if (checkInput(ref)) {
+                formIsValid = false;
+            }
+        });
+
+        if (formIsValid) {
+            setIsFormSubmitted(true);
+            resetForm();
+        }
     }
 
-    function displayErrors() {
-      return   errors.map((error)=>{
-          console.log(error.keys.contains('name'));
-            return <li key={Object.keys(error)[0]}> {Object.keys(error)}   :  {error[Object.keys(error)[0]]} </li>
-        })
+    const resetForm = () => {
+        name.current.value = '';
+        email.current.value = '';
+        message.current.value = '';
+        conditions.current.checked = false;
     }
 
-
-    return <div className="container mt-3">
-        {isFormSubmited ? <div className="alert alert-success" role="alert">
-            <h4 className="alert-heading">Success !!</h4>
-            <p></p>
-            <p className="mb-0">Form Submited successfuly !!</p>
-        </div> : ''}
-        {errors.length > 0 ?
-            <div className="alert alert-danger" role="alert">
-                <h4 className="alert-heading">Errors</h4>
-                <ul>
-                    {displayErrors()}
-                </ul>
-            </div> : ''}
-        <h1>Validation Form:</h1>
-        <form onSubmit={handelSubmit}>
-            <div className="form-group mb-2">
-                <label htmlFor="name">Name:</label>
-                <input ref={name} type="text" name="name" id="name" className="form-control"
-                       placeholder="Enter Your name" aria-describedby="nameHelp"/>
-                <small id="nameHelp" className="text-black"> { errors.name  ? <span className="text-danger">Name :  {errors.name} </span> : "Please enter a valid name"} </small>
-            </div>
-            <div className="form-group mb-2">
-            <label htmlFor="email">Email:</label>
-                <input ref={email} type="text" name="email" id="email" className="form-control"
-                       placeholder="Enter Your email" aria-describedby="emailHelp"/>
-                <small id="emailHelp" className="text-muted"> {errors.email  ? <span className="text-danger">Email :  {errors.email} </span> : "Please enter a valid" } </small>
-            </div>
-            <div className="form-group mb-2">
-                <label htmlFor="message">Message:</label>
-                <textarea ref={message} name="message" id="message" className="form-control"
-                          placeholder="Enter Your message" aria-describedby="messageHelp"></textarea>
-                <small id="messageHelp" className="text-muted"> { errors.message  ? <span className="text-danger">message :  {errors.message} </span> : "Please enter a valid message"}</small>
-            </div>
-            <div className="form-group mb-2 d-flex align-items-center">
-                <input ref={conditions} type="checkbox" id="conditions"/>
-                <label htmlFor="conditions" className="text-muted mx-2">Terms and conditions : <span className="text-danger"> { errors.conditions ?? '' } </span> </label>
-            </div>
-            <div className="form-group mb-2 d-flex align-items-center justify-content-center">
-                <button type="submit" className="btn btn-info w-100">Send</button>
-            </div>
-        </form>
-    </div>
+    return (
+        <div className="container mt-3">
+            {isFormSubmitted ? (
+                <div className="alert alert-success" role="alert">
+                    <h4 className="alert-heading">Success !! </h4>
+                    <p className="mb-0">Form Submitted successfully !!</p>
+                </div>
+            ) : ''}
+            {Object.keys(errors).length > 0 && (
+                <div className="alert alert-danger" role="alert">
+                    <h4 className="alert-heading">Errors</h4>
+                    <ul>
+                        {Object.entries(errors).map(([key, value]) => (
+                            <li key={key}>{key} : {value}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <h1>Validation Form:</h1>
+            <form onSubmit={handelSubmit}>
+                <div className="form-group mb-2">
+                    <label htmlFor="name">Name:</label>
+                    <input ref={name} type="text" name="name" id="name" className="form-control"
+                           placeholder="Enter Your name" aria-describedby="nameHelp"/>
+                    <small id="nameHelp" className="text-muted"> { errors.name  ? <span className="text-danger">Name :  {errors.name} </span> : "Please enter a valid name"} </small>
+                </div>
+                <div className="form-group mb-2">
+                    <label htmlFor="email">Email:</label>
+                    <input ref={email} type="text" name="email" id="email" className="form-control"
+                           placeholder="Enter Your email" aria-describedby="emailHelp"/>
+                    <small id="emailHelp" className="text-muted"> {errors.email  ? <span className="text-danger">Email :  {errors.email} </span> : "Please enter a valid" } </small>
+                </div>
+                <div className="form-group mb-2">
+                    <label htmlFor="message">Message:</label>
+                    <textarea ref={message} name="message" id="message" className="form-control"
+                              placeholder="Enter Your message" aria-describedby="messageHelp"></textarea>
+                    <small id="messageHelp" className="text-muted"> { errors.message  ? <span className="text-danger">message :  {errors.message} </span> : "Please enter a valid message"}</small>
+                </div>
+                <div className="form-group mb-2 d-flex align-items-center">
+                    <input ref={conditions} type="checkbox" id="conditions"/>
+                    <label htmlFor="conditions" className="text-muted mx-2">Terms and conditions : <span className="text-danger"> { errors.conditions ?? '' } </span> </label>
+                </div>
+                <div className="form-group mb-2 d-flex align-items-center justify-content-center">
+                    <button type="submit" className="btn btn-info w-100">Send</button>
+                </div>
+            </form>
+        </div>
+    );
 }
